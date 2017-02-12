04a159b416d07ae28afce587b0da2f53a6d5ba1c
oleksii <oleksii@oleksii.debian.home>
2017-02-11 21:48:39 -0500

    take as candidate anotated with ActionTypes classes and interfaces

--- src/main/java/com/liberty/soge/Runner.java
+++ src/main/java/com/liberty/soge/Runner.java
@@ -1,11 +1,13 @@
 package com.liberty.soge;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.liberty.soge.config.ActionTypesConfig;
 import com.liberty.soge.config.BaseConfig;
 import com.liberty.soge.config.WebSocketConfig;
 import com.liberty.soge.config.quartz.JobConfig;
 import com.liberty.soge.config.quartz.QuartzConfig;
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * @author Dmytro_Kovalskyi.
@@ -17,7 +19,7 @@ public class Runner {
     public static void main(String[] args) {
         ConfigurableApplicationContext context =
                 SpringApplication.run(new Class<?>[]{BaseConfig.class, WebSocketConfig.class,
-                        JobConfig.class, QuartzConfig.class}, args);
+                        JobConfig.class, QuartzConfig.class, ActionTypesConfig.class}, args);
 
         System.out.println("Application started...");
     }
