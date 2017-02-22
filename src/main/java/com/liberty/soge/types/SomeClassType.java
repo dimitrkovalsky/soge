c14be960d1d70e73f636527c47623e8ed237ab19
Oleksii Bobko <skellag@gmail.com>
2017-02-22 11:46:46 +0200

    on startup in case if duplicate actions detected throw RuntimeException

--- src/main/java/com/liberty/soge/types/SomeClassType.java
+++ src/main/java/com/liberty/soge/types/SomeClassType.java
@@ -1,13 +1,13 @@
 package com.liberty.soge.types;
 
-import com.liberty.soge.action.test.SecondTestAction;
+import com.liberty.soge.action.test.ThirdTestAction;
 import com.liberty.soge.annotation.ActionTypes;
 import com.liberty.soge.annotation.Handler;
 
 @ActionTypes
 public class SomeClassType {
     
-    @Handler(SecondTestAction.class)
+    @Handler(ThirdTestAction.class)
     static int RT_TEST_2 = 10;
     
 }
